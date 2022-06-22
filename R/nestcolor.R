9c8996228aefb4334647fc5959aae689158ba6c2
arkadiusz.beer <arkadiusz.beer@contractors.roche.com>
2022-06-22 15:53:33 +0200

    fixes

---
+++ R/nestcolor.R
@@ -0,0 +1,5 @@
+#' @description
+#' `nestcolor` provides a custom [`pkgdown`](https://pkgdown.r-lib.org)
+#' color for NEST packages. Please don’t use it for your own package.
+#' @keywords internal
+"_PACKAGE"
