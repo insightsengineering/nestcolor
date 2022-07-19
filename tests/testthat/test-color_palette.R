553a4f69569f7bf63959d692430538d0e6043200
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2022-07-19 14:21:31 -0400

    Update documentation for new palette

--- tests/testthat/test-color_palette.R
+++ tests/testthat/test-color_palette.R
@@ -1,10 +1,11 @@
-test_that("color_palette works for all three supported palettes", {
+test_that("color_palette works for all four supported palettes", {
   # Get 10 color values for each palette
   nest_pal <- color_palette(n = 10, palette = "nest")
   stream_pal <- color_palette(n = 10, palette = "stream")
   viridis_pal <- color_palette(n = 10, palette = "viridis")
+  access_pal <- color_palette(n = 10, palette = "accessible")
 
-  result <- c(nest_pal, stream_pal, viridis_pal)
+  result <- c(nest_pal, stream_pal, viridis_pal, access_pal)
 
   # Compare
   expected <- c(
@@ -13,7 +14,9 @@ test_that("color_palette works for all three supported palettes", {
     "#343cff", "#ff484b", "#232323", "#329032", "#ffa41c",
     "#750375", "#1d831c", "#767676", "#9b2525", "#008080",
     "#0D0887", "#FB9E3B", "#FDC927", "#D8576C", "#47039F",
-    "#BE3686", "#47039F", "#7301A8", "#ED7953", "#9C179E"
+    "#BE3686", "#47039F", "#7301A8", "#ED7953", "#9C179E",
+    "#E8F086", "#6FDE6E", "#FF4242", "#A691AE", "#235FA4",
+    "#0A284B", "#BDD9BF", "#929084", "#FFC857", "#A997DF"
   )
   expect_identical(result, expected)
 })
