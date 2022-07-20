f77b4b99f541da9832c8a7ebb828384f1c80a697
Emily de la Rua <59304861+edelarua@users.noreply.github.com>
2022-07-20 11:01:06 -0400

    19 Add graphing packages as downstream dependencies (#20)
    * Add packages as downstream dependencies
    
    * Add ggplot.discrete.fill to zzz.R file
    
    * [skip actions] Restyle files
    
    * Update zzz
    
    * manage join conflicts
    
    * [skip actions] Restyle files
    
    * Fix lintr
    
    * [skip actions] Restyle files
    
    * Add chevron to downstream dependencies
    
    * Update NEWS
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dawid Kałędkowski <dawid.kaledkowski@gmail.com>

---
+++ R/scale_nest_discrete.R
@@ -0,0 +1,65 @@
+#' @export
+scale_nest_manual <- function(aesthetics,
+                              scale_name,
+                              palette = stream_pallette,
+                              name = waiver(),
+                              breaks = waiver(),
+                              labels = waiver(),
+                              limits = NULL,
+                              expand = waiver(),
+                              na.translate = TRUE, # nolint
+                              na.value = NA, # nolint
+                              drop = TRUE,
+                              guide = "legend",
+                              position = "left",
+                              super = ggplot2::ScaleDiscrete) {
+  browser()
+  discrete_scale(
+    aesthetics = aesthetics,
+    scale_name = scale_name,
+    palette = stream_pallette,
+    name = name,
+    breaks = breaks,
+    labels = labels,
+    limits = limits,
+    expand = expand,
+    na.translate = na.translate,
+    na.value = na.value,
+    drop = drop,
+    guide = guide,
+    position = position,
+    super = super
+  )
+}
+
+scale_nest_discrete <- function(aesthetics,
+                                scale_name,
+                                palette,
+                                name = waiver(),
+                                breaks = waiver(),
+                                labels = waiver(),
+                                limits = NULL,
+                                expand = waiver(),
+                                na.translate = TRUE, # nolint
+                                na.value = NA, # nolint
+                                drop = TRUE,
+                                guide = "legend",
+                                position = "left",
+                                super = ggplot2::ScaleDiscrete) {
+  discrete_scale(
+    aesthetics = aesthetics,
+    scale_name = scale_name,
+    palette = stream_pallette,
+    name = name,
+    breaks = breaks,
+    labels = labels,
+    limits = limits,
+    expand = expand,
+    na.translate = na.translate,
+    na.value = na.value,
+    drop = drop,
+    guide = guide,
+    position = position,
+    super = super
+  )
+}
